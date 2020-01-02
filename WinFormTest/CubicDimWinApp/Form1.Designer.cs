7fa73917f46a70a1d27acce661cb8352a7ef3c98
treknchaz <56511550+treknchaz@users.noreply.github.com>
2020-01-02 17:54:53 +0200

    Add files via upload

---
+++ WinFormTest/CubicDimWinApp/Form1.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace CubicDimWinApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.widthLabel = new System.Windows.Forms.Label();
+            this.widthtTextBox = new System.Windows.Forms.TextBox();
+            this.heightTextBox = new System.Windows.Forms.TextBox();
+            this.heightLabel = new System.Windows.Forms.Label();
+            this.lengthTextBox = new System.Windows.Forms.TextBox();
+            this.lengthLabel = new System.Windows.Forms.Label();
+            this.calcButton = new System.Windows.Forms.Button();
+            this.calcTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // widthLabel
+            // 
+            this.widthLabel.AutoSize = true;
+            this.widthLabel.BackColor = System.Drawing.Color.Transparent;
+            this.widthLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.widthLabel.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.widthLabel.Location = new System.Drawing.Point(29, 217);
+            this.widthLabel.Name = "widthLabel";
+            this.widthLabel.Size = new System.Drawing.Size(61, 20);
+            this.widthLabel.TabIndex = 0;
+            this.widthLabel.Text = "Width";
+            // 
+            // widthtTextBox
+            // 
+            this.widthtTextBox.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.widthtTextBox.Location = new System.Drawing.Point(44, 181);
+            this.widthtTextBox.Name = "widthtTextBox";
+            this.widthtTextBox.Size = new System.Drawing.Size(31, 20);
+            this.widthtTextBox.TabIndex = 1;
+            this.widthtTextBox.TextChanged += new System.EventHandler(this.widthtTextBox_TextChanged);
+            // 
+            // heightTextBox
+            // 
+            this.heightTextBox.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.heightTextBox.Location = new System.Drawing.Point(43, 240);
+            this.heightTextBox.Name = "heightTextBox";
+            this.heightTextBox.Size = new System.Drawing.Size(30, 20);
+            this.heightTextBox.TabIndex = 3;
+            // 
+            // heightLabel
+            // 
+            this.heightLabel.AutoSize = true;
+            this.heightLabel.BackColor = System.Drawing.Color.Transparent;
+            this.heightLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.heightLabel.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.heightLabel.Location = new System.Drawing.Point(23, 278);
+            this.heightLabel.Name = "heightLabel";
+            this.heightLabel.Size = new System.Drawing.Size(67, 20);
+            this.heightLabel.TabIndex = 2;
+            this.heightLabel.Text = "Height";
+            // 
+            // lengthTextBox
+            // 
+            this.lengthTextBox.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.lengthTextBox.Location = new System.Drawing.Point(44, 301);
+            this.lengthTextBox.Name = "lengthTextBox";
+            this.lengthTextBox.Size = new System.Drawing.Size(29, 20);
+            this.lengthTextBox.TabIndex = 5;
+            this.lengthTextBox.TextChanged += new System.EventHandler(this.lengthTextBox_TextChanged);
+            // 
+            // lengthLabel
+            // 
+            this.lengthLabel.AutoSize = true;
+            this.lengthLabel.BackColor = System.Drawing.Color.Transparent;
+            this.lengthLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lengthLabel.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lengthLabel.Location = new System.Drawing.Point(23, 158);
+            this.lengthLabel.Name = "lengthLabel";
+            this.lengthLabel.Size = new System.Drawing.Size(71, 20);
+            this.lengthLabel.TabIndex = 4;
+            this.lengthLabel.Text = "Length";
+            // 
+            // calcButton
+            // 
+            this.calcButton.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.calcButton.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.calcButton.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calcButton.Location = new System.Drawing.Point(8, 327);
+            this.calcButton.Name = "calcButton";
+            this.calcButton.Size = new System.Drawing.Size(108, 34);
+            this.calcButton.TabIndex = 6;
+            this.calcButton.Text = "Calculate =";
+            this.calcButton.UseVisualStyleBackColor = false;
+            this.calcButton.Click += new System.EventHandler(this.calcButton_Click);
+            // 
+            // calcTextBox
+            // 
+            this.calcTextBox.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.calcTextBox.Location = new System.Drawing.Point(134, 327);
+            this.calcTextBox.Name = "calcTextBox";
+            this.calcTextBox.Size = new System.Drawing.Size(65, 20);
+            this.calcTextBox.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(134, 285);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Result";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(478, 252);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(618, 252);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label4.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label4.Location = new System.Drawing.Point(597, 338);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(21, 48);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "\r\n?";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.textBox1.Location = new System.Drawing.Point(574, 198);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(66, 20);
+            this.textBox1.TabIndex = 14;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.button1.Location = new System.Drawing.Point(574, 299);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label2.Location = new System.Drawing.Point(531, 179);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(161, 16);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Enter Tempature Here";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label5.Location = new System.Drawing.Point(519, 224);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 13);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "From";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label6.Location = new System.Drawing.Point(672, 224);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(22, 13);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "To";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(36, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(703, 86);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "CONVERSION TABLE";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.calcButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.BackgroundImage = global::CubicDimWinApp.Properties.Resources.n;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.calcTextBox);
+            this.Controls.Add(this.calcButton);
+            this.Controls.Add(this.lengthTextBox);
+            this.Controls.Add(this.lengthLabel);
+            this.Controls.Add(this.heightTextBox);
+            this.Controls.Add(this.heightLabel);
+            this.Controls.Add(this.widthtTextBox);
+            this.Controls.Add(this.widthLabel);
+            this.Name = "Form1";
+            this.Text = "Conversion Box";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label widthLabel;
+        private System.Windows.Forms.TextBox widthtTextBox;
+        private System.Windows.Forms.TextBox heightTextBox;
+        private System.Windows.Forms.Label heightLabel;
+        private System.Windows.Forms.TextBox lengthTextBox;
+        private System.Windows.Forms.Label lengthLabel;
+        private System.Windows.Forms.Button calcButton;
+        private System.Windows.Forms.TextBox calcTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WinFormTest/CubicDimWinApp/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CubicDimWinApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            string[] a = { "celsius", "fahrenheit" };
+            string[] b = { "fahrenheit", "celsius" };
+            comboBox1.DataSource = a;
+            comboBox2.DataSource = b;
+
+            this.lengthTextBox.KeyDown += LengthTextBox_KeyDown;
+        }
+
+        private void LengthTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Back || e.KeyData == Keys.Enter)
+            {
+                int result = calc();
+                calcTextBox.Text = Convert.ToString(result);
+            }
+        }
+
+        private void calcButton_Click(object sender, EventArgs e)
+        {
+            int result = calc();
+            calcTextBox.Text = Convert.ToString(result);
+
+        }
+
+        int calc ()
+        {
+            return int.Parse(widthtTextBox.Text) * int.Parse(heightTextBox.Text) * int.Parse(lengthTextBox.Text);
+        }
+        private void lengthTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+       
+        private void widthtTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double startingTemp, resultTemp;
+            startingTemp = double.Parse(textBox1.Text);
+            if (comboBox1.Text == "celsius" && comboBox2.Text == "fahrenheit") ;
+            {
+                resultTemp = (startingTemp * (1.8) + 32);
+                label4.Text = resultTemp.ToString();
+            }
+            if (comboBox1.Text == "fahrenheit" && comboBox2.Text == "celsius") ;
+            {
+                resultTemp = ((startingTemp - 32) / 1.8);
+                label4.Text = resultTemp.ToString();
+            }
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+    }
+}
